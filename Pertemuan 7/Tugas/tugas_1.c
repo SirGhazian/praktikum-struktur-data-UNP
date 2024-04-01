94ab96296fd25ceac2c0bd3af0e7829a129951f2
naizahg <142916107+SirGhazian@users.noreply.github.com>
2024-04-01 20:31:28 +0700

    Update tugas_1.c

--- Pertemuan 7/Tugas/tugas_1.c
+++ Pertemuan 7/Tugas/tugas_1.c
@@ -1,7 +1,6 @@
-// Algoritma BFS dalam Bahasa C
-
 #include <stdio.h>
 #include <stdlib.h>
+
 #define SIZE 40
 
 struct queue {
@@ -13,7 +12,6 @@ struct queue {
 struct queue* createQueue();
 void enqueue(struct queue* q, int);
 int dequeue(struct queue* q);
-void display(struct queue* q);
 int isEmpty(struct queue* q);
 void printQueue(struct queue* q);
 
